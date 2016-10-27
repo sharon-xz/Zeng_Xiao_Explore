f6c4cc4ebab63f15a5b42491240af96763c43af6
xz339 <XZ@XIaos-MacBook-Pro.local>
2016-10-27 16:04:06 -0400

    adding content
    .

--- explore_Zeng_Xiao.R
+++ explore_Zeng_Xiao.R
@@ -1,4 +1,5 @@
 library(plyr)
+data(diamonds)
 
 explore <- function(dataframe, switch, cutoff_value, bin_value){
   explore_list <- list("explore")
@@ -21,79 +22,71 @@ summary_table <- function(dataframe){
   return(lapply(dataframe[,column],summary))
 }
 
+
 rsquare <- function(dataframe){
   name_pairs <- cbind()  #Initiation
-  rsquared <- cbind()
+  rsquare <- cbind()
+  i <- 0
+  columns <- dataframe[sapply(dataframe, is.numeric)]
   
-  for (i in (1:ncol(dataframe))){
-  name1 = names(diamonds)[i]  #getting the first column name
-  
-  for (j in (i+1:ncol(dataframe))){  #only getting the columns after the first one
-    if  (is.na(names(diamonds)[j]) == FALSE){
-      if (names(diamonds)[j] != names(diamonds)[i]){
-        name2 =names(diamonds)[j]}   #getting the second column name
-      name_pairs <- rbind(name_pairs,paste(name1,name2,sep = "-", collapse = NULL))  #paste them in the "name1-name2" format
-      
-      if (is.numeric(dataframe[,name1]) == TRUE){  #only calculates the correlation for numeric columns
-        if (is.numeric(dataframe[,name2]) == TRUE){
-          linear_model<- lm(dataframe[,name1] ~ dataframe[,name2]) 
-          rsquared<- rbind(rsquared, summary(linear_model)$r.squared)}#calculating the pearson correlations
-        else{
-          rsquared<- rbind(rsquared, NA)  #for non-numeric columns, the correletion is NA to avoid errors
-        }
-      }
-      else{
-        rsquared<- rbind(rsquared, NA)
-      }
-      
+  if(ncol(columns) >= 2) {
+    pairs <- combn(colnames(columns), 2) 
+    
+    name_pairs <- paste(pairs[1,], pairs[2, ], sep = "-") 
+    for (i in (1:ncol(pairs))){
+      x <- pairs[1,i]
+      y <- pairs[2,i]
+      lmodel <- lm(dataframe[x]~ dataframe[y])
+      rsquare <- rbind(rsquare,summary(lmodel)$r.squared)
     }
+    
+    #rsquared <- paste(summary(lm(dataframe[pairs[1,]]~ dataframe$pairs[[2,]]))$r.squared)
+    
+    
+    #rsquared <- lapply(lm(pairs[1,]~ pairs[2,]), summary$r.squared)
+    
+    result <- data.frame(pairs, rsquared) 
+    return(result)
+    
   }
-
+  else{
+    print("R-squared cannot be computted because there are not enough numeric columns")}
 }
 
-output = data.frame(name_pairs,rsquared) #combining two columns
-return(output)
-}
+
+
 
 correlation <- function(dataframe,cutoff_value){
-  #This function accept any dataframe as a parameter and returns a dataframe that contains each pair of column names in the first column in a single string separated by a -, 
-  #and their corresponding Pearson correlation coefficient in the second column.
-  #Parameters: A dataframe
-  #Returns: A dataframe
   
-  name_pairs <- cbind()  #Initiation
-  correlations <- cbind()
-  
-  for (i in (1:ncol(dataframe))){
-    name1 = names(diamonds)[i]  #getting the first column name
     
-    for (j in (i+1:ncol(dataframe))){  #only getting the columns after the first one
-      if  (is.na(names(diamonds)[j]) == FALSE){
-        if (names(diamonds)[j] != names(diamonds)[i]){
-          name2 =names(diamonds)[j]}   #getting the second column name
-          name_pairs <- rbind(name_pairs,paste(name1,name2,sep = "-", collapse = NULL))  #paste them in the "name1-name2" format
-        
-        if (is.numeric(dataframe[,name1]) == TRUE){  #only calculates the correlation for numeric columns
-          if (is.numeric(dataframe[,name2]) == TRUE){
-            correlations<- rbind(correlations, cor(dataframe[,name1],dataframe[,name2], method="pearson"))}#calculating the pearson correlations
-          else{
-            correlations<- rbind(correlations, NA)  #for non-numeric columns, the correletion is NA to avoid errors
-          }}
-        else{
-          correlations<- rbind(correlations, NA)
-        }
-        
-      }
+    columns <- dataframe[sapply(dataframe, is.numeric)] 
+    
+    
+    if(ncol(columns) >= 2) {
+      b <- combn(colnames(columns), 2) #finds all combinations of the name pairs
+      
+      pairs <- paste(b[1,], b[2, ], sep = "-") 
+      
+      c <- cor(columns, method = "pearson")
+      
+      correlation <- c[which(lower.tri(c))] 
+      
+      result <- data.frame(pairs, correlation) #create a new data frame with our pairs 
+      return(result)
+      
     }
-    
+    else  #print this message if we can't find Pearson correlation
+      print("Pearson Correlation cannot be computted because there are not enough numeric columns")
   }
+
+
+plots <- function(dataframe, switch, bin_value){
   
-  output = data.frame(name_pairs,correlations) #combining two columns
-  return(output)
+  f_columns <- dataframe[sapply(dataframe, is.factor)]
+  graph<-ggplot(dataframe,aes(f_columns, colour = "#808080")) 
+  
+  graph1<-ggplot(dataframe,aes(price, colour = "#0000ff" )) +geom_histogram(binwidth = bin_value) 
+  graph2<-ggplot(dataframe,aes(density(price), colour = "#0000ff")) +geom_histogram(binwidth = bin_value) 
 }
-
-
-
-plots <- function(dataframe, switch){}
 
 
